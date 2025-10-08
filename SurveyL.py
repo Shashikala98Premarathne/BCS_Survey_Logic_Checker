7317ed894af3af00e2c90f18c911c0df35a913e2
Shashikala98Premarathne <shashi98nimashi@gmail.com>
2025-10-08 13:30:25 +0200

    Commit 17

--- SurveyL.py
+++ SurveyL.py
@@ -216,10 +216,10 @@ VARIABLE_STRUCTURE = {
     "consideration_b": {
         "suffix_range": [15,19,21,27,28,32,38,47,58,98],
         "allowed_values": [0,1],
-    },
-    "last_purchase_b": {
-        "suffix_range": [15,19,21,27,28,32,38,47,58,98],
-        "allowed_values": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,99],
+#    },
+#    "last_purchase_b": {
+#        "suffix_range": [15,19,21,27,28,32,38,47,58,98],
+#        "allowed_values": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,99],
     },
     "last_workshop_visit_b": {
         "suffix_range": [15,19,21,27,28,32,38,47,58,98],
